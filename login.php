94606bb8cbcd1396760a9ca0defae5e146c4dca0
Pusaneko • ᜉᜒᜎᜒᜉᜒᜈᜐ᜔ <90695286+pusanekoph@users.noreply.github.com>
2022-02-10 23:10:23 +0800

    Add files via upload

---
+++ login.php
@@ -0,0 +1,27 @@
+<html>
+    <head>
+        <title>Aklat Baybayin | Pagpasok</title>
+    </head>
+    <style>
+        @font-face{font-family: Tagalog Stylized;src: url("font/ts.ttf");}
+        #bar{height:100px;background-color:#b5651d;color:white;padding: 4px;text-align: center;border-radius: 10px}
+        #signup_button{background-color: #90EE90;width: 70px;text-align: center;padding: 4px;border-radius: 4px;margin-left: 370px;}
+        #login_bar{background-color: skyblue;color:white;width: 800px;height: auto; font-weight:bold;margin: auto;margin-top: 20px;padding: 10px;border-radius:10px;padding-top: 20px;text-align: center;}
+        #text{height: 25px;width: 300px; border-radius: 5px;border: solid 1px #aaa;padding: 4px;font-size: 14px;}
+        #button{height: 25px;width: 300px;border-radius: 5px;border: none;background-color: #90EE90;color: white;}
+</style>
+    <body style="font-family: Tagalog Stylized">
+        <div id="bar">
+            <div style="font-size:40px;margin-top: 15px;">ᜀᜃ᜔ᜎᜆ᜔ ᜊᜌ᜔ᜊᜌᜒᜈ᜔</div>
+            <div id="signup_button">ᜄᜓᜋᜏ</div>
+        </div>
+        <div>
+            <div id="login_bar">
+                ᜉᜓᜋᜐᜓᜃ᜔ ᜐ ᜀᜃ᜔ᜎᜆ᜔ ᜊᜌ᜔ᜊᜌᜒᜈ᜔<br><br>
+                <input type="text" id="text" placeholder="ᜐᜓᜎᜆ᜔ᜇᜓᜈᜒᜃᜓ"><br><br>
+                <input type="password" id="text" placeholder="ᜉᜐ᜔ᜏᜓᜇ᜔ᜇ᜔"><br><br>
+                <input type="submit" id="button" value="ᜉᜓᜋᜐᜓᜃ᜔"><br><br>
+            </div>
+        </div>
+    </body>
+</html>
